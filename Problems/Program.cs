2e131c883326f8a790bc0acf385c7d36de9b30e4
Freewillie <staplewm@gmail.com>
2019-02-21 15:50:50 +0200

    reset

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,24 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static string Prefix(string input)
+        {
+            string output = "";
+            int num = 0, words = 0;
+            foreach(char c in input){
+                num++; 
+                if(c.equals(' '))
+                word++;
+            }
+            output = $"{num},{word}:{input}"
+            return output;
         }
     }
 }
